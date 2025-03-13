f1870d15390751446b9dd828deffce417264c4ec
Johey Hargrett <joheyhargrett@gmail.com>
2025-03-13 16:26:04 -0400

    pract reduce redo lab

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const reducer = (accumulator, element) => {
+    return accumulator + element;
+}
+
+let totalBatteries = batteryBatches.reduce(reducer, 0);
+
+
+console.log(totalBatteries);
+
+
+
